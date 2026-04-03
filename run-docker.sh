326f9225989bf08d1d8115452344e2989d6b19b1
Rubén Nieto <ruben.nieto@urjc.es>
2026-04-03 13:19:22 +0200

    Fix: Configuración de rutas Xilinx, PYTHONPATH y gestión segura de contenedores en servidor

--- run-docker.sh
+++ run-docker.sh
@@ -71,9 +71,9 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 # the settings below will be taken from environment variables if available,
 # otherwise the defaults below will be used
-: ${JUPYTER_PORT=8888}
+: ${JUPYTER_PORT=8898}
 : ${JUPYTER_PASSWD_HASH=""}
-: ${NETRON_PORT=8081}
+: ${NETRON_PORT=8082}
 : ${LOCALHOST_URL="localhost"}
 : ${PYNQ_USERNAME="xilinx"}
 : ${PYNQ_PASSWORD="xilinx"}
@@ -182,65 +182,83 @@ if [ "$FINN_DOCKER_PREBUILT" = "0" ]; then
   cd $OLD_PWD
 fi
 # Launch container with current directory mounted
-# important to pass the --init flag here for correct Vivado operation, see:
-# https://stackoverflow.com/questions/55733058/vivado-synthesis-hangs-in-docker-container-spawned-by-jenkins
-DOCKER_EXEC="docker run -t --rm $DOCKER_INTERACTIVE --tty --init "
-DOCKER_EXEC+="--hostname $DOCKER_INST_NAME "
-DOCKER_EXEC+="-e SHELL=/bin/bash "
-DOCKER_EXEC+="-w $SCRIPTPATH "
-DOCKER_EXEC+="-v $SCRIPTPATH:$SCRIPTPATH "
-DOCKER_EXEC+="-v $FINN_HOST_BUILD_DIR:$FINN_HOST_BUILD_DIR "
-DOCKER_EXEC+="-e FINN_BUILD_DIR=$FINN_HOST_BUILD_DIR "
-DOCKER_EXEC+="-e FINN_ROOT="$SCRIPTPATH" "
-DOCKER_EXEC+="-e LOCALHOST_URL=$LOCALHOST_URL "
-DOCKER_EXEC+="-e VIVADO_IP_CACHE=$VIVADO_IP_CACHE "
-DOCKER_EXEC+="-e PYNQ_BOARD=$PYNQ_BOARD "
-DOCKER_EXEC+="-e PYNQ_IP=$PYNQ_IP "
-DOCKER_EXEC+="-e PYNQ_USERNAME=$PYNQ_USERNAME "
-DOCKER_EXEC+="-e PYNQ_PASSWORD=$PYNQ_PASSWORD "
-DOCKER_EXEC+="-e PYNQ_TARGET_DIR=$PYNQ_TARGET_DIR "
-DOCKER_EXEC+="-e OHMYXILINX=$OHMYXILINX "
-DOCKER_EXEC+="-e NUM_DEFAULT_WORKERS=$NUM_DEFAULT_WORKERS "
-if [ "$FINN_DOCKER_RUN_AS_ROOT" = "0" ];then
-  DOCKER_EXEC+="-v /etc/group:/etc/group:ro "
-  DOCKER_EXEC+="-v /etc/passwd:/etc/passwd:ro "
-  DOCKER_EXEC+="-v /etc/shadow:/etc/shadow:ro "
-  DOCKER_EXEC+="-v /etc/sudoers.d:/etc/sudoers.d:ro "
-  DOCKER_EXEC+="-v $FINN_SSH_KEY_DIR:$HOME/.ssh "
-  DOCKER_EXEC+="--user $DOCKER_UID:$DOCKER_GID "
-else
-  DOCKER_EXEC+="-v $FINN_SSH_KEY_DIR:/root/.ssh "
-fi
-if [ ! -z "$IMAGENET_VAL_PATH" ];then
-  DOCKER_EXEC+="-v $IMAGENET_VAL_PATH:$IMAGENET_VAL_PATH "
-  DOCKER_EXEC+="-e IMAGENET_VAL_PATH=$IMAGENET_VAL_PATH "
-fi
-if [ ! -z "$FINN_XILINX_PATH" ];then
-  VIVADO_PATH="$FINN_XILINX_PATH/Vivado/$FINN_XILINX_VERSION"
-  VITIS_PATH="$FINN_XILINX_PATH/Vitis/$FINN_XILINX_VERSION"
-  HLS_PATH="$FINN_XILINX_PATH/Vitis_HLS/$FINN_XILINX_VERSION"
-  DOCKER_EXEC+="-v $FINN_XILINX_PATH:$FINN_XILINX_PATH "
-  if [ -d "$VIVADO_PATH" ];then
-    DOCKER_EXEC+="-e "XILINX_VIVADO=$VIVADO_PATH" "
-    DOCKER_EXEC+="-e VIVADO_PATH=$VIVADO_PATH "
-  fi
-  if [ -d "$HLS_PATH" ];then
-    DOCKER_EXEC+="-e HLS_PATH=$HLS_PATH "
-  fi
-  if [ -d "$VITIS_PATH" ];then
-    DOCKER_EXEC+="-e VITIS_PATH=$VITIS_PATH "
-  fi
-  if [ -d "$PLATFORM_REPO_PATHS" ];then
-    DOCKER_EXEC+="-v $PLATFORM_REPO_PATHS:$PLATFORM_REPO_PATHS "
-    DOCKER_EXEC+="-e PLATFORM_REPO_PATHS=$PLATFORM_REPO_PATHS "
-    DOCKER_EXEC+="-e ALVEO_IP=$ALVEO_IP "
-    DOCKER_EXEC+="-e ALVEO_USERNAME=$ALVEO_USERNAME "
-    DOCKER_EXEC+="-e ALVEO_PASSWORD=$ALVEO_PASSWORD "
-    DOCKER_EXEC+="-e ALVEO_BOARD=$ALVEO_BOARD "
-    DOCKER_EXEC+="-e ALVEO_TARGET_DIR=$ALVEO_TARGET_DIR "
-  fi
-fi
-DOCKER_EXEC+="$FINN_DOCKER_EXTRA "
-DOCKER_EXEC+="$FINN_DOCKER_TAG $DOCKER_CMD"
+# ==============================================================================
+# BLOQUE DE LANZAMIENTO SEGURO (CON PATH DE CONDA Y XILINX)
+# ==============================================================================
 
-$DOCKER_EXEC
+# 1. DEFINIR EL TAG (Asegúrate de que coincide con el de 'docker images')
+FINAL_TAG="xilinx/finn:5762f54-dirty.xrt_202210.2.13.466_18.04-amd64-xrt"
+
+# 2. CONFIGURAR EL PATH INTERNO (Inyectando Xilinx y manteniendo Conda)
+# Añadimos /opt/conda/bin que es donde vive Jupyter y Pip en esta imagen
+INTERNAL_PATH="/opt/conda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
+if [ ! -z "$FINN_XILINX_PATH" ]; then
+    VIVADO_PATH="$FINN_XILINX_PATH/Vivado/$FINN_XILINX_VERSION"
+    HLS_PATH="$FINN_XILINX_PATH/Vitis_HLS/$FINN_XILINX_VERSION"
+    
+    # Añadimos los bins de Xilinx al principio del PATH
+    if [ -d "$HLS_PATH/bin" ]; then
+        INTERNAL_PATH="$HLS_PATH/bin:$INTERNAL_PATH"
+    fi
+    if [ -d "$VIVADO_PATH/bin" ]; then
+        INTERNAL_PATH="$VIVADO_PATH/bin:$INTERNAL_PATH"
+    fi
+fi
+
+# 3. INICIALIZAR EL ARRAY DE ARGUMENTOS DE DOCKER
+DOCKER_RUN_CMD=(docker run -t --rm --tty --init)
+
+if [ ! -z "$DOCKER_INTERACTIVE" ]; then
+    DOCKER_RUN_CMD+=($DOCKER_INTERACTIVE)
+fi
+
+# 4. AÑADIR VARIABLES DE ENTORNO Y VOLÚMENES
+# Importante: mantenemos PYTHONPATH para que encuentre tus librerías locales
+DOCKER_RUN_CMD+=(
+    --hostname "$DOCKER_INST_NAME"
+    -e "SHELL=/bin/bash"
+    -w "$SCRIPTPATH"
+    -v "$SCRIPTPATH:$SCRIPTPATH"
+    -v "$FINN_HOST_BUILD_DIR:$FINN_HOST_BUILD_DIR"
+    -v "$FINN_XILINX_PATH:$FINN_XILINX_PATH"
+    -e "FINN_BUILD_DIR=$FINN_HOST_BUILD_DIR"
+    -e "FINN_ROOT=$SCRIPTPATH"
+    -e "XILINX_VIVADO=$VIVADO_PATH"
+    -e "VIVADO_PATH=$VIVADO_PATH"
+    -e "HLS_PATH=$HLS_PATH"
+    -e "PATH=$INTERNAL_PATH"
+    -e "PYTHONPATH=$SCRIPTPATH/src:$SCRIPTPATH/deps/qonnx/src:$SCRIPTPATH/deps/brevitas:$SCRIPTPATH/deps/finn-experimental/src"
+    -e "LOCALHOST_URL=$LOCALHOST_URL"
+    -e "NUM_DEFAULT_WORKERS=$NUM_DEFAULT_WORKERS"
+)
+
+# 5. PUERTOS JUPYTER / NETRON
+if [ "$1" = "notebook" ]; then
+    DOCKER_RUN_CMD+=(
+        -e "JUPYTER_PORT=$JUPYTER_PORT"
+        -e "NETRON_PORT=$NETRON_PORT"
+        -p "$JUPYTER_PORT:$JUPYTER_PORT"
+        -p "$NETRON_PORT:$NETRON_PORT"
+    )
+fi
+
+# 6. CONFIGURACIÓN DE USUARIO (Si no es root)
+if [ "$FINN_DOCKER_RUN_AS_ROOT" = "0" ]; then
+    DOCKER_RUN_CMD+=(
+        -v "/etc/group:/etc/group:ro"
+        -v "/etc/passwd:/etc/passwd:ro"
+        -v "/etc/shadow:/etc/shadow:ro"
+        -v "/etc/sudoers.d:/etc/sudoers.d:ro"
+        --user "$DOCKER_UID:$DOCKER_GID"
+    )
+fi
+
+# MENSAJE DE ESTADO
+gecho "-------------------------------------------------------"
+gecho "Lanzando FINN..."
+gecho "PATH corregido: $INTERNAL_PATH"
+gecho "-------------------------------------------------------"
+
+# 7. EJECUCIÓN FINAL
+"${DOCKER_RUN_CMD[@]}" "$FINAL_TAG" $DOCKER_CMD
